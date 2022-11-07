0d626be1fb6c155a33de4d17b08fc20b5105ec01
lince098 <pablogamarro@hotmail.com>
2022-11-07 19:20:11 +0100

    Update main.py: Eliminación de puntos y comas accidentales

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import time
 
 def fibonacci_1(x):
     if x < 0:
-        raise ValueError("x no puede ser menor que 0");
+        raise ValueError("x no puede ser menor que 0")
     if x < 2:
         return x
     else:
-        actual = 0;
-        siguiente = 1;
+        actual = 0
+        siguiente = 1
         for _ in range(x):
             temp = actual + siguiente
             actual = siguiente
@@ -18,7 +18,7 @@ def fibonacci_1(x):
 
 def fibonacci_2(x):
     if x < 0:
-        raise ValueError("x no puede ser menor que 0");
+        raise ValueError("x no puede ser menor que 0")
     if x < 2:
         return x
     return fibonacci_2(x - 1) + fibonacci_2(x - 2)
@@ -29,7 +29,7 @@ cache = {0: 0, 1: 1}
 
 def fibonacci_3(x):
     if x < 0:
-        raise ValueError("x no puede ser menor que 0");
+        raise ValueError("x no puede ser menor que 0")
     if x in cache:
         return cache[x]
     else:
@@ -38,6 +38,7 @@ def fibonacci_3(x):
         return z
 
 
-start_time = time.time()
-print(fibonacci_3(9))
-print("--- %s seconds ---" % (time.time() - start_time))
+if __name__ == "__main__":
+    start_time = time.time()
+    print(fibonacci_3(9))
+    print("--- %s seconds ---" % (time.time() - start_time))
