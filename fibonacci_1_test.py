462177c387e75553582c48702d2bd93cae6170e9
lince098 <pablogamarro@hotmail.com>
2022-11-07 19:29:09 +0100

    Test para las 3 funciones fibonacci con unittest

---
+++ fibonacci_1_test.py
@@ -0,0 +1,19 @@
+from main import fibonacci_1 as fibonacci
+import unittest
+
+
+class TestFibonacci_1(unittest.TestCase):
+
+    def test_fibonacci_1_is_1(self):
+        self.assertEqual(1, fibonacci(1))
+
+    def test_fibonacci_9_is_34(self):
+        self.assertEqual(34, fibonacci(9))
+
+    def test_fibonacci_(self):
+        with self.assertRaises(ValueError):
+            fibonacci(-10)
+
+
+if __name__ == "__main__":
+    unittest.main()
